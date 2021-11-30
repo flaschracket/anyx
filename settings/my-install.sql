68dd0b5cf4623936f0648ce0ba55c5924c32df4a
flaschracket <52179120+flaschracket@users.noreply.github.com>
2021-11-30 11:25:42 +0100

    Delete settings directory

--- settings/my-install.sql
+++
@@ -1,4 +0,0 @@
-INSERT IGNORE INTO `engine4_core_modules` (`name`, `title`, `description`, `version`, `enabled`, `type`) VALUES  ('anyx', 'Anyx interface', 'the interface that help other resources to integrate with a social engine', '5.0.0', 1, 'extra') ;
-
-
-ALTER TABLE `engine4_users` ADD `relational_id` INT(11) NULL AFTER `user_id`;
